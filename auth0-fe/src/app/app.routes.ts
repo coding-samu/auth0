ad793f756d721f0351da36e8fd9e307e897ad87c
Samuele Quaresima <s.quaresima@jpanik.it>
2025-02-11 11:39:15 +0100

    Updated app router following HomeComponent, GroupListComponent and GroupEditComponent updates

--- auth0-fe/src/app/app.routes.ts
+++ auth0-fe/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { GroupListComponent } from './group-list/group-list.component';
-import {GroupEditComponent} from './group-edit/group-edit.component';
+import { HomeComponent } from './pages/home.component';
+import { GroupListComponent } from './pages/group-list/group-list.component';
+import {GroupEditComponent} from './pages/group-edit/group-edit.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
